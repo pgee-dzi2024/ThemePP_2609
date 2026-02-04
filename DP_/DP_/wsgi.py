eb12f4edb2cf27d1ecad93105bb97062fe9a9d26
pgee-dzi2024 <pgee_2024@abv.bg>
2026-02-04 11:15:22 +0200

    Чиста инсталация на Django

--- DP_/DP_/wsgi.py
+++ DP_/DP_/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for DP project.
+WSGI config for DP_ project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DP_.settings')
 
 application = get_wsgi_application()
